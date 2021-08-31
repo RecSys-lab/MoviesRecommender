a21fdd27676bd0859ecbf4def0c6034d15826e83
Ali Tourani <a.tourani1991@gmail.com>
2021-08-31 22:43:27 +0430

    feat: add video file check and fixed-length frame

--- Frames/frameExtractor.py
+++ Frames/frameExtractor.py
@@ -7,41 +7,56 @@ from Frames.resizeTools.frameResize import frameResize
 # This module extracts frames from a given list of movies
 def frameExtractor(moviesDirectory, outputDirectory, networkInputSize):
     print(f'Fetching the list of items in "{moviesDirectory}"')
-    videoFiles = os.listdir(moviesDirectory)
-    print(f'Number of videos: {len(videoFiles)}')
-    for file in videoFiles:
-        print(f'Processing video {file} ...')
-        # Accessing video and provide a proper name for it
-        currentVideoPath = f'{moviesDirectory}/{file}'
-        normalizedVideoName = file.split('.')
-        normalizedVideoName = string.capwords(
-            normalizedVideoName[0].replace("_", "")).replace(" ", "")
-        # Creating output folder
-        generatedPath = outputDirectory + '/' + normalizedVideoName
-        if not os.path.exists(generatedPath):
-            os.mkdir(generatedPath)
-        # Capturing video
-        capturedVideo = cv2.VideoCapture(currentVideoPath)
-        frameRate = capturedVideo.get(cv2.CAP_PROP_FPS)
-        success, image = capturedVideo.read()
-        # Calculating the aspect-ratio
-        print(f'Extracting frames (one frame in every {frameRate} frames) ...')
-        frameCounter = 0
-        fileNameCounter = 0
-        while success:
-            if (frameCounter % int(frameRate) == 0):
-                # Resizing the image, while preserving its aspect-ratio
-                # image = squareFrameGenerator(image, networkInputSize) # In case we need a square frame
-                image = frameResize(image, networkInputSize)
-                # Save the frame as a file
-                cv2.imwrite(
-                    f"{outputDirectory}/{normalizedVideoName}/frame{fileNameCounter}.jpg", image)
-                fileNameCounter += 1
+    try:
+        videoFiles = os.listdir(moviesDirectory)
+        # Filter only video files
+        for file in videoFiles:
+            if not file.lower().endswith(('.mkv', '.avi', '.mp4')):
+                videoFiles.remove(file)
+        print(f'Number of videos: {len(videoFiles)}')
+        # Iterate on all video files in the given directory
+        for file in videoFiles:
+            print(f'Processing video {file} ...')
+            # Accessing video and provide a proper name for it
+            currentVideoPath = f'{moviesDirectory}/{file}'
+            normalizedVideoName = file.split('.')
+            normalizedVideoName = string.capwords(
+                normalizedVideoName[0].replace("_", "")).replace(" ", "")
+            # Creating output folder
+            generatedPath = outputDirectory + '/' + normalizedVideoName
+            if not os.path.exists(generatedPath):
+                os.mkdir(generatedPath)
+            # Capturing video
+            capturedVideo = cv2.VideoCapture(currentVideoPath)
+            frameRate = int(capturedVideo.get(cv2.CAP_PROP_FPS))
             success, image = capturedVideo.read()
-            # Showing progress
-            if (frameCounter % 1000 == 0):
-                currentTime = int(frameCounter / frameRate)
-                print(
-                    f'Prcessing frame #{frameCounter} ({currentTime:,} seconds passed) ...')
-            frameCounter += 1
-        print(f'Frames generated for {normalizedVideoName} in {generatedPath}')
+            # Calculating the aspect-ratio
+            print(
+                f'Extracting frames (one frame in every {frameRate} frames) ...')
+            frameCounter = 0
+            fileNameCounter = 0
+            while success:
+                if (frameCounter % frameRate == 0):
+                    # Resizing the image, while preserving its aspect-ratio
+                    # image = squareFrameGenerator(image, networkInputSize) # In case we need a square frame
+                    image = frameResize(image, networkInputSize)
+                    # Format the frame counter as: frame1 --> frame0000001
+                    formattedFrameCounter = '{0:07d}'.format(fileNameCounter)
+                    # Save the frame as a file
+                    cv2.imwrite(
+                        f"{outputDirectory}/{normalizedVideoName}/frame{formattedFrameCounter}.jpg", image)
+                    fileNameCounter += 1
+                success, image = capturedVideo.read()
+                # Showing progress
+                if (frameCounter % 1000 == 0):
+                    currentTime = int(frameCounter / frameRate)
+                    print(
+                        f'Prcessing frame #{frameCounter} ({currentTime:,} seconds passed) ...')
+                frameCounter += 1
+            print(
+                f'Frames generated for {normalizedVideoName} in {generatedPath}')
+    except FileNotFoundError:
+        print(
+            "🚨 [FileNotFoundError] The input directory does not exist or contain video files!")
+    except Exception as e:
+        print('🚨 Error while running the app:', str(e))
