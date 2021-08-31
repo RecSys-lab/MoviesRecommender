59877d4739cd7c0cdac31a973ffeaf4aa6ea86d9
Ali Tourani <a.tourani1991@gmail.com>
2021-09-01 00:22:11 +0430

    feat: add a sample config

---
+++ config.example.py
@@ -0,0 +1,11 @@
+import os
+
+# Dataset Generation
+moviesList = 'C:/Some/Path/Dataset.csv'
+generatedDataset = os.path.abspath('./Data/generated/output.csv')
+movieLenzRatings = 'C:/Some/Path/ratings.csv'
+
+# Feature Extraction
+networkInputSize = 420
+moviesDirectory = 'C:/Some/Path/'
+outputDirectory = 'C:/Some/Path/'
