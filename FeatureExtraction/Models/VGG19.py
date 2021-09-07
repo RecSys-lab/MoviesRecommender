bf48121ca791a3f414f0957d1cf31ecd7189edc1
Ali Tourani <a.tourani1991@gmail.com>
2021-09-08 00:56:01 +0430

    fix: [temporary] enable VGG-19

--- FeatureExtraction/Models/VGG19.py
+++ FeatureExtraction/Models/VGG19.py
@@ -1,4 +1,4 @@
-# from keras import Model
+from keras import Model
 from keras.applications.vgg19 import VGG19, preprocess_input
 from keras.preprocessing.image import load_img, img_to_array
 
@@ -13,20 +13,20 @@ tempImage = 'E:\Datasets\For Test\Movie Frames\00000123\frame0000096.jpg'
 
 def VGG19Launcher():
     # Load a frame and convert it into a numpy array
-    # frame = load_img(tempImage, target_size=(vggInputSize, vggInputSize))
-    # frame = img_to_array(frame)
-    # # Reshape the image according to the model's structure
-    # frame = frame.reshape((1, frame.shape[0], frame.shape[1], frame.shape[2]))
-    # # Preprocessing
-    # frame = preprocess_input(frame)
-    # # Load model
+    frame = load_img(tempImage, target_size=(vggInputSize, vggInputSize))
+    frame = img_to_array(frame)
+    # Reshape the image according to the model's structure
+    frame = frame.reshape((1, frame.shape[0], frame.shape[1], frame.shape[2]))
+    # Preprocessing
+    frame = preprocess_input(frame)
+    # Load model
     print('\n🚀 Launching VGG-19 network ...')
-    # model = VGG19()
-    # # Removing the final output layer, so that the second last fully connected layer with 4,096 nodes will be the new output layer
-    # model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
-    # # Get extracted features
-    # features = model.predict(frame)
-    # print('🔥 Features are ready! Check the output path!')
-    # print(features.shape)
+    model = VGG19()
+    # Removing the final output layer, so that the second last fully connected layer with 4,096 nodes will be the new output layer
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    # Get extracted features
+    features = model.predict(frame)
+    print('🔥 Features are ready! Check the output path!')
+    print(features.shape)
     # save to file
     # dump(features, open('dog.pkl', 'wb'))
